9949285a453f0b5211db2b44b3733178d0aeb35a
mahwin <mahwin7085@gmail.com>
2022-09-06 23:58:07 +0900

    Refactory dispatch wrapper Fn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { createStore } from "./redux.js";
 
-//data가 참조형이 아니라서 밖에 state는 영향을 안 받기 때문에 return 해줘여 함.
+const COUNTER = "counter"; // 액션타입
 
+//data가 참조형이 아니라서 밖에 state는 영향을 안 받기 때문에 return 해줘여 함.
 function reducer(state, action) {
-  if (action.type === "count") {
+  if (action.type === COUNTER) {
     //다른 값들은 그대로 들어가게 스프레드 연산자 쓰고 counter부분만 오버 라이드 되게 !
     return { ...state, counter: action.payload.counter };
   }
@@ -19,15 +20,22 @@ function handler() {
   console.log(store.getState());
 }
 
-store.add(handler);
+store.subscribe(handler);
 
 //묶어주는 데이터를 페이로드라고 많이 부름.
-store.dispatch({
-  type: "count",
-  payload: {
-    counter: 1,
-  },
-});
+
+function actionCreator(type, payload) {
+  return {
+    type,
+    payload,
+  };
+}
+
+function couter(data) {
+  store.dispatch(actionCreator("counter", { data }));
+}
+
+couter({ counter: 1 });
 
 // updator => 내가 원하는 형태로 함수를 바꿔줘라고 함수를 푸쉬 할 수 있음.
 // get으로 값을 꺼내는 이유는 진짜 state를 받게 되면 변경할 수 있기 떄문임.
